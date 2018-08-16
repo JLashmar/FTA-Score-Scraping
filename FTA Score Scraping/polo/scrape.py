11bfdecbc7c418937b7c1b854edfbfd9dc24e597
JLashmar <jordan.lashmar@outlook.com>
2018-08-16 20:58:02 +0100

    scraping, sending to CVS. Just need to fix the score apperance

--- FTA Score Scraping/polo/scrape.py
+++ FTA Score Scraping/polo/scrape.py
@@ -5,64 +5,64 @@ import csv
 source = requests.get('https://www.pololine.com/tournament/').text
 soup = BeautifulSoup(source, 'lxml')
 
-# csv_file = open('cms_scrape.csv', 'w')
+csv_file = open('cms_scrape.csv', 'w')
 
-# csv_writer = csv.writer(csv_file)
-# csv_writer.writerow(['Competition', 'Link'])
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Competition', 'Match Date', 'Team A', '"Team B', 'Score'])
 
-# for comp_name in soup.find_all('a', class_='eg-zone-element-1'):
-#    competition = comp_name.text
-#    link = comp_name['href']
-#    print(competition)
-#    print(link)
-# print(comp_page)
+for comp_name in soup.find_all('a', class_='eg-zone-element-1'):
+    competition = comp_name.text
+    link = comp_name['href']
+    print(competition)
+    print(link)
 
-#    csv_writer.writerow([competition, link])
+    next_level_link = requests.get(link).text  # basically source
+    bs = BeautifulSoup(next_level_link, 'lxml')  # basically soup
 
-comp_link = soup.find('a', class_='eg-zone-element-1')
-link = comp_link['href']
+    # inside each page
+    for data in bs.find_all('tr', 'sp-row'):
+        # match_date
+        try:
+            match_date = data.date.text
+            match_date = match_date.split(' ')[0]
+            date = match_date.split('-')
+            date = '%s-%s-%s' % (date[2], date[1], date[0])
+            # print(date)
+        except Exception as e:
+            pass
 
+        # team names
+        try:
+            for team_name in data.find_all('td', 'data-teams'):
+                mylist = []
+                for names in team_name.find_all('a'):
+                    name = names.text
+                    mylist.append(name)
+                # print(mylist)
+                team1 = mylist[0]
+                team2 = mylist[1]
+                # print(team2)
+        except Exception as e:
+            pass
 
-next_level_link = requests.get(link).text  # basically source
-bs = BeautifulSoup(next_level_link, 'lxml')  # basically soup
+        # score
+        try:
+            scorelist = []
+            for score in data.find_all('td', 'data-results'):
+                score = score.text
+                scorelist.append(score)
+                print(scorelist)
+                if score == '-':
+                    list = [competition, date, team1, team2, '0', '0']
+                    csv_writer.writerow(list)
+                    # print(list)
+                else:
+                    list = [competition, date, team1, team2, score]
+                    csv_writer.writerow(list)
+                    # print(list)
+        except Exception as e:
+            pass
 
-# inside each page
-for data in bs.find_all('tr', 'sp-row'):
-    # match_date
-    try:
-        match_date = data.date.text
-        match_date = match_date.split(' ')[0]
-        date = match_date.split('-')
-        date = '%s-%s-%s' % (date[2], date[1], date[0])
-        print(date)
-    except Exception as e:
-        pass
+csv_file.close
 
-    # team names
-    try:
-        for team_name in data.find_all('td', 'data-teams'):
-            for names in team_name.find_all('a'):
-                name = names.text
-                name = name
-                print(name)
-    except Exception as e:
-        pass
-
-    # score
-    try:
-        for score in data.find_all('td', 'data-results'):
-            score = score.text
-            if score == '-':
-                print('0')
-            else:
-                print(score)
-    except Exception as e:
-        pass
-
-    # print(data.prettify())
-
-
-# for info in comp_link.find('li'):
-# print(info)
-
-# csv_file.close
+print('done')
---
+++ FTA Score Scraping/polo/test_scrape.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://www.pololine.com/tournament/').text
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Competition', 'Match Date', 'Team A', 'Team B', 'Score'])
+
+comp_name = soup.find('a', class_='eg-zone-element-1')
+competition = comp_name.text
+link = comp_name['href']
+# print(competition)
+# print(link)
+
+comp_link = soup.find('a', class_='eg-zone-element-1')
+link = comp_link['href']
+
+next_level_link = requests.get('https://www.pololine.com/tournaments/joe-barry-cup/').text  # basically source
+bs = BeautifulSoup(next_level_link, 'lxml')  # basically soup
+
+# inside each page
+for data in bs.find_all('tr', 'sp-row'):
+    # match_date
+    try:
+        match_date = data.date.text
+        match_date = match_date.split(' ')[0]
+        date = match_date.split('-')
+        date = '%s-%s-%s' % (date[2], date[1], date[0])
+        # print(date)
+    except Exception as e:
+        pass
+
+    # team names
+    try:
+        for team_name in data.find_all('td', 'data-teams'):
+            mylist = []
+            for names in team_name.find_all('a'):
+                name = names.text
+                mylist.append(name)
+            # print(mylist)
+            team1 = mylist[0]
+            team2 = mylist[1]
+            # print(team2)
+    except Exception as e:
+        pass
+
+    # score
+    try:
+        scorelist = []
+        for score in data.find_all('td', 'data-results'):
+            score = score.text
+            scorelist.append(score)
+            scorelist.split('-')
+            print(scorelist)
+            if score == '-':
+                list = [competition, date, team1, team2, '0']
+                csv_writer.writerow(list)
+            else:
+                list = [competition, date, team1, team2, score]
+                csv_writer.writerow(list)
+    except Exception as e:
+        pass
+
+csv_file.close
+
+print('done')
